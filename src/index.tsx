e059629b96a92d611e748a7a3b10dd44858a9b75
Francesco Giordano <francesco.giordano@buildo.io>
2017-04-07 16:00:01 +0200

    hello

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-import React from 'react'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class Hello extends React.Component<{}, void> {
+  render() {
+    return <div>Hello</div>
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
